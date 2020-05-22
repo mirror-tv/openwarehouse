1bc7e01df106913cb41990766172024b4ea2f79f
andyfcx <dreamcastle.andy@gmail.com>
2020-05-22 18:30:04 +0800

    [Chg] implement gcsfile typing

--- fields/GCSFile/Implementation.js
+++ fields/GCSFile/Implementation.js
@@ -1,17 +1,34 @@
-const { Field } = require('@keystonejs/fields');
+import {keystone} from "../../index";
+
+const { File } = require('@keystonejs/fields');
+// const fs = require('fs');
+const CloudStorageAdapter = require('keystone-storage-adapter-cloud-storage')
+const keyFilename = require('./configs/gcskeyfile')
+const {KnexAdapter} = require('@keystonejs/adapter-knex')
+
+// options = {"keyFilename": keyFilename}
+
+var cloudStorageAdapter = CloudStorageAdapter({keyFilename:keyFilename})
 
 class GCSFile extends File.implementation {
-  constructor(path, { starCount = 5 }) {
+  constructor(path) {
     super(...arguments);
-    this.starCount = starCount;
+    this.keystone = keystone;
+    this.fileAdapter = cloudStorageAdapter;
+
+
   }
 
+
+
   extendAdminMeta(meta) {
-    return { ...meta, starCount: this.starCount };
+    return { ...meta };
   }
 }
 
+
+
 module.exports = {
-  Stars,
-  KnexIntegerInterface: Integer.adapters.knex,
+  GCSFile,
+  KnexInterface,
 };
