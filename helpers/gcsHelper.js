2c0f65e4a406e11437598ffdf38285b2307ffbad
andyhuang <andyhuang@mirrormedia.mg>
2020-05-27 13:46:14 +0800

    [Chg] rewrite GCSAdapter in reference to S3Adapter

--- helpers/gcsHelper.js
+++ helpers/gcsHelper.js
@@ -1,19 +1,19 @@
 var gcloud = require('gcloud');
 
-module.exports.getPublicUrl = function (bucket, filename) {
+getPublicUrl = function (bucket, filename) {
 	return `https://storage.googleapis.com/{bucket}/{filename}`;
 };
 
-module.exports.uploadFileToBucket = function (bucket, fileReadStream, options) {
+uploadFileToBucket = function (bucket, fileReadStream, options) {
 	return new Promise(function (resolve, reject) {
 		var opts = options || {};
 		var file = bucket.file(opts.destination);
         var metadata = {};
-        
+
        if (opts.filetype) {
            metadata.contentType = opts.filetype;
        }
-       
+
       if (opts.cacheControl) {
           metadata.cacheControl = opts.cacheControl;
       }
@@ -26,7 +26,7 @@ module.exports.uploadFileToBucket = function (bucket, fileReadStream, options) {
 				reject(err);
 			})
 			.on('finish', function () {
-				console.log('finish uploading to bucket with destionation:', opts.destination);
+				console.log('finish uploading to bucket with destination:', opts.destination);
 				file.makePublic(function (err, apiResponse) {
 					if (err) {
 						return reject(err);
@@ -37,7 +37,7 @@ module.exports.uploadFileToBucket = function (bucket, fileReadStream, options) {
 	});
 };
 
-module.exports.makeFilePublicPrivateRead = function (file, isPublicRead) {
+makeFilePublicPrivateRead = function (file, isPublicRead) {
 	return new Promise(function (resolve, reject) {
 		if (isPublicRead) {
 			file.makePublic(function (err, apiResponse) {
@@ -57,7 +57,7 @@ module.exports.makeFilePublicPrivateRead = function (file, isPublicRead) {
 	});
 };
 
-module.exports.initBucket = function (config, bucket) {
+initBucket = function (config, bucket) {
 	var gcs = gcloud.storage({
 		projectId: config.projectId,
 		keyFilename: config.keyFilename,
@@ -66,7 +66,7 @@ module.exports.initBucket = function (config, bucket) {
 	return gcs.bucket(bucket);
 };
 
-module.exports.getGcsFiles = function (bucket, filename) {
+getGcsFiles = function (bucket, filename) {
 	return new Promise(function (resolve, reject) {
 		bucket.getFiles({
 			prefix: filename,
@@ -78,3 +78,6 @@ module.exports.getGcsFiles = function (bucket, filename) {
 		});
 	});
 };
+
+
+module.exports = {getPublicUrl, uploadFileToBucket, makeFilePublicPrivateRead, initBucket, getGcsFiles}
