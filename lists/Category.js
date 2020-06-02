e520a8cd4afe2a75b9715d7e161a73c9bd2e51b2
Leon Peng <leonpeng@mirrormedia.mg>
2020-06-02 15:01:12 +0800

    adjust Category list and its related access-control

--- lists/Category.js
+++ lists/Category.js
@@ -1,54 +1,35 @@
-const { Text, Checkbox, Select } = require('@keystonejs/fields');
+const { Slug, Text, Checkbox, Relationship } = require('@keystonejs/fields');
 const { atTracking, byTracking } = require('@keystonejs/list-plugins');
+const access = require('../helpers/access');
 
 module.exports = {
     fields: {
-        name: {
-            label: "名稱",
-            type: Text,
+        slug: {
+            label: "Slug",
+            type: Slug,
             isRequired: true,
             isUnique: true
         },
         title: {
-            label: "中文名稱",
+            label: "標題",
             type: Text,
             isRequired: true
         },
-        //heroImage: { label: '首圖', type: Types.ImageRelationship, ref: 'Image' },
-        og_title: {
-            label: 'FB分享標題',
+        ogTitle: {
+            label: 'FB 分享標題',
             type: Text
         },
-        og_description: {
-            label: 'FB分享說明',
+        ogDescription: {
+            label: 'FB 分享說明',
             type: Text
         },
-        //og_image: { label: 'FB分享縮圖', type: Types.ImageRelationship, ref: 'Image' },
-        css: {
-            label: 'CSS',
-            type: Text,
-            isMultiline: true
-        },
-        javascript: {
-            label: 'JavaScript',
-            type: Text,
-            isMultiline: true
-        },
-        style: {
-            type: Select,
-            options: 'feature, listing, tile',
-            defaultValue: 'feature'
-        },
-        isAudioSiteOnly: {
-            label: '僅用於語音網站',
-            type: Checkbox
+        ogImage: {
+            label: 'FB 分享縮圖',
+            type: Relationship,
+            ref: 'Image'
         },
         isFeatured: {
             label: '置頂',
-            type: Checkbox
-        },
-        isCampaign: {
-            label: '活動分類',
             type: Checkbox
         },
     },
@@ -56,7 +37,13 @@ module.exports = {
         atTracking(),
         byTracking(),
     ],
+    access: {
+        update: access.userIsAdminOrModeratorOrOwner,
+        create: access.userIsNotContributor,
+        delete: access.userIsAdminOrModeratorOrOwner,
+    },
     adminConfig: {
-        defaultColumns: 'title, name',
+        defaultColumns: 'slug, title, isFeatured, createdAt',
+        defaultSort: '-createdAt',
     },
 }
