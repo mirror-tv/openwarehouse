8bc4f536225ddcc729cefb3fc1ddcf315996837c
Leon Peng <leonpeng@mirrormedia.mg>
2020-06-10 07:44:12 +0800

    upgrade draft-js to ^0.11.6 and resolve partial pasted html issue

--- fields/HTML/views/Field.js
+++ fields/HTML/views/Field.js
@@ -3,6 +3,7 @@ import { jsx } from '@emotion/core';
 import { FieldContainer } from '@arch-ui/fields';
 import { EditorState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
+import { handleDraftEditorPastedText } from "draftjs-conductor";
 import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 const HtmlField = ({ onChange, autoFocus, field, value, errors }) => {
@@ -13,9 +14,22 @@ const HtmlField = ({ onChange, autoFocus, field, value, errors }) => {
             <Editor
                 editorState={editorState}
                 onEditorStateChange={onChange}
+                handlePastedText={handlePastedText}
             />
         </FieldContainer>
     );
 };
+
+const handlePastedText = (text, html, editorState, onChange) => {
+    if (html) {
+        let newState = handleDraftEditorPastedText(html, editorState);
+        if (newState) {
+            onChange(newState);
+            return true;
+        }
+    }
+
+    return false;
+}
 
 export default HtmlField;
