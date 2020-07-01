c81721390e665b0c4d8f1c7ba1f1e895e159c16b
Baron Chiu <baronchiu@mirrormedia.mg>
2020-07-01 15:27:21 +0800

    wip: initial commit of annotation decorator

---
+++ fields/HTML/views/editor/decorators/annotationDecorator.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { AnnotationType } from '../controls/Annotation';
+import classNames from 'classnames';
+
+function strategy(contentBlock, callback, contentState) {
+    contentBlock.findEntityRanges(character => {
+        const entityKey = character.getEntity();
+        console.log("AnnotationType", AnnotationType);
+        return (
+            entityKey !== null &&
+            contentState.getEntity(entityKey).getType() === AnnotationType
+        );
+    }, callback);
+}
+
+const component = (props) => {
+    const { annotation, pureAnnotationText } = props.contentState.getEntity(props.entityKey).getData();
+    const [expanded, setExpanded] = useState(false);
+    return (
+        <abbr
+            className="annotation"
+            title={pureAnnotationText}
+            style={{ cursor: 'pointer', borderBottom: 0 }}
+        >
+            <span
+                onClick={() => setExpanded(!expanded)}
+                style={{ color: 'red' }}
+            >
+                {pureAnnotationText}
+                <span className={classNames(expanded ? 'up' : '', 'indicator')} />
+            </span>
+            <span
+                className="annotation-html"
+                dangerouslySetInnerHTML={{ __html: annotation }}
+                style={{
+                    display: expanded ? 'block' : 'none',
+                    backgroundColor: 'white',
+                    padding: '16px',
+                    fontSize: '15px',
+                    lineHeight: 1.5,
+                }}
+            />
+        </abbr>
+    );
+}
+
+export default { strategy: strategy, component: component };
--- fields/HTML/views/editor/decorators/index.js
+++ fields/HTML/views/editor/decorators/index.js
@@ -1,9 +1,11 @@
 import youtubeDecorator from './youtubeDecorator';
 import embedCodeDecorator from './embedCodeDecorator';
 import blockQuoteDecorator from './blockQuoteDecorator';
+import annotationDecorator from './annotationDecorator';
 
 
 export default [
+    annotationDecorator,
     youtubeDecorator,
     embedCodeDecorator,
     blockQuoteDecorator,
