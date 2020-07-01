1d28c5c2e575fdfbf38948731eaeefa57602f8f9
Baron Chiu <baronchiu@mirrormedia.mg>
2020-07-01 15:26:28 +0800

    wip: insert annotation to editorState

--- fields/HTML/views/editor/controls/Annotation/index.js
+++ fields/HTML/views/editor/controls/Annotation/index.js
@@ -1,20 +1,75 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { EditorState, convertToRaw } from 'draft-js';
+import { EditorState, convertToRaw, Modifier } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
-import { getSelectedBlock } from "draftjs-utils";
+import { getSelectedBlock, getSelectionEntity, getEntityRange } from "draftjs-utils";
 
 
 import MiniEditor from '../../components/MiniEditor';
 import '../../css/main.css';
 
-const Annotation = (props) => {
+export const AnnotationType = "ANNOTATION";
+export default (props) => {
+    const { editorState, onChange } = props;
 
-    const { editorState } = props;
+    const generateContentBlock = () => {
+        const selectedType = getSelectedBlocksType(editorState);
+
+        switch (selectedType) {
+            case AnnotationType:
+            // extract text and content block
+            default:
+            // use as text and generate a content block
+        }
+    }
 
     const getPreSelectedText = () => getSelectedBlock(editorState).getText();;
     // TODO customDecorators
-    const onSave = (text, contentState) => console.log("onSave:", text, draftToHtml(convertToRaw(contentState)));;
+
+    const onSave = (text, contentState) => {
+        const currentEntity = editorState ? getSelectionEntity(editorState) : undefined;
+
+        let selection = editorState.getSelection();
+
+        if (currentEntity) {
+            const entityRange = getEntityRange(editorState, currentEntity);
+            const isBackward = selection.getIsBackward();
+            if (isBackward) {
+                selection = selection.merge({
+                    anchorOffset: entityRange.end,
+                    focusOffset: entityRange.start,
+                });
+            } else {
+                selection = selection.merge({
+                    anchorOffset: entityRange.start,
+                    focusOffset: entityRange.end,
+                });
+            }
+        }
+        let currentContentState = editorState.getCurrentContent();
+        currentContentState = Modifier.splitBlock(currentContentState, selection);
+        currentContentState = currentContentState.createEntity(AnnotationType, 'IMMUTABLE', {
+            pureAnnotationText: text,
+            annotation: draftToHtml(convertToRaw(contentState)),
+        });
+        const entityKey = currentContentState.getLastCreatedEntityKey();
+
+        currentContentState = Modifier.replaceText(
+            currentContentState,
+            selection,
+            ' ',
+            undefined,
+            entityKey
+        );
+
+        const newEditorState = EditorState.push(
+            editorState,
+            currentContentState,
+            'insert-characters'
+        );
+
+        onChange(newEditorState);
+    };
 
     return (
         <MiniEditor
@@ -40,5 +95,3 @@ const prepareLayoutConfig = () => ({
         last: true
     }
 });
-
-export default Annotation;
