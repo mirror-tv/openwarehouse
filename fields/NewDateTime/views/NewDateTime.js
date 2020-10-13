4233e230f13b0d70641ae460150675d158ac81c4
“liyibass” <liyibass@mirrormedia.mg>
2020-10-13 19:24:33 +0800

    fix unix timestamp issue due to different structure between postgres and CMS.

--- fields/NewDateTime/views/NewDateTime.js
+++ fields/NewDateTime/views/NewDateTime.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './NewDateTime.style.css'
 import Datetime from 'react-datetime'
 import 'react-datetime/css/react-datetime.css'
@@ -7,27 +7,37 @@ import './DateFormat'
 function NewDateTime({ value, onChange }) {
   const [inputField, setInputField] = useState('')
 
+  // react-datetime accept integer unix timestamp
+  // this part is for future adjustment
+  console.log(typeof value)
+  const newValue = parseInt(value)
+
+  // get selected unix timestamp by moment from callback
   const changeHandler = (moment) => {
-    console.log(moment)
-    const selectDateTime = moment._d.format('MM-dd-yyyy hh:mm')
-    setInputField(selectDateTime)
-    onChange(selectDateTime)
+    const selectUnixTimestamp = parseInt(moment.format('x'))
+
+    setInputField(selectUnixTimestamp)
+    onChange(selectUnixTimestamp.toString())
   }
+
+  // get current unix timestamp
   const nowHandler = (e) => {
     e.preventDefault()
-    const nowDateTime = new Date().format('MM-dd-yyyy hh:mm')
-    setInputField(nowDateTime)
-    onChange(nowDateTime)
+    const nowUnixTimestamp = Date.now()
+
+    setInputField(nowUnixTimestamp)
+    onChange(nowUnixTimestamp.toString())
   }
 
   return (
     <div className="NewDateTime">
       <Datetime
         value={inputField}
-        initialValue={value ? value : ''}
+        initialValue={newValue ? newValue : ''}
         onChange={(moment) => changeHandler(moment)}
-        dateFormat="MM-DD-YYYY"
-        timeFormat="HH:mm"
+
+        // dateFormat="YYYY-MM-DD"
+        // timeFormat="HH:mm:ss"
       />
       <button className="nowButton" onClick={(e) => nowHandler(e)}>
         Now
