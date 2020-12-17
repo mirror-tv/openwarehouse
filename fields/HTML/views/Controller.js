e669d990d48a5c2d7fe10037b3ff27760a92b7ad
“liyibass” <liyibass@mirrormedia.mg>
2020-12-17 17:18:15 +0800

    refactor: revert HTML field Controller path, update draft version.

--- fields/HTML/views/Controller.js
+++ fields/HTML/views/Controller.js
@@ -3,6 +3,7 @@ import {
     convertDbDataToEditorState,
     convertEditorStateToDbData,
 } from './editorToBackendUtil/dataConverter'
+import {convertToRaw} from 'draft-js'
 
 // controller defines how front-end features work
 class HtmlController extends FieldController {
@@ -12,15 +13,15 @@ class HtmlController extends FieldController {
 
     // when save post, format data from editorState to specified object, then return to db.
     serialize = (data) => {
-        return data.content
-            ? JSON.stringify(convertEditorStateToDbData(data.content))
+        return data[this.path]
+            ? JSON.stringify(convertEditorStateToDbData(data[this.path]))
             : undefined
     }
 
     // when lode post, format data from db object to editorState, then return to editor.
     deserialize = (data) => {
         return convertDbDataToEditorState(
-            data.content ? JSON.parse(data.content) : undefined
+            data[this.path] ? JSON.parse(data[this.path]) : undefined
         )
     }
 
