285d375d1a69c2d57c263c8e8204aef3a0ddb2d7
liyibass <liyibass@gmail.com>
2021-07-09 09:24:55 +0800

    feat: add customize Search component in hook

--- hooks/mirror-tv.js
+++ hooks/mirror-tv.js
@@ -1,9 +1,18 @@
 // import customedItemHeaderActions from './customedItemHeaderActions';
 import React from 'react'
 import K5Logo from './components/K5Logo/K5Logo'
-import { ItemId, AddNewItem } from '@keystonejs/app-admin-ui/components'
+import {
+    ItemId,
+    AddNewItem,
+    CreateItem,
+} from '@keystonejs/app-admin-ui/components'
 import PreviewBtn from './components/PreviewBtn/PreviewBtn'
 import './style/hooks.style.css'
+
+import Search from './components/Search/Search'
+
+import { useList } from '@keystonejs/app-admin-ui/client/providers/List'
+
 export default {
     // custom landing page logo
     logo: () => <K5Logo />,
@@ -13,6 +22,15 @@ export default {
                 <ItemId />
                 <PreviewBtn />
                 <AddNewItem />
+            </div>
+        )
+    },
+    listHeaderActions: () => {
+        const { list } = useList()
+        return (
+            <div className="list-header-actions">
+                <Search list={list} />
+                <CreateItem />
             </div>
         )
     },
