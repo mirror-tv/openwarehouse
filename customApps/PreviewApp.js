b344add3b74c5d48285ab7170596876dc7fb2339
liyibass <liyibass@gmail.com>
2021-07-14 04:43:47 +0800

    feat: add additional router which handle huxt's corresponding file & api

--- customApps/PreviewApp.js
+++ customApps/PreviewApp.js
@@ -6,6 +6,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 class PreviewApp {
     constructor({ path }) {
         this._path = path
+        // this.proxyTarget = process.env.K5_PREVIEW_URL || 'http://localhost:3001',
+        this.proxyTarget = process.env.K5_PREVIEW_URL || 'https://dev.mnews.tw'
     }
 
     getPreviewRouter() {
@@ -22,10 +24,10 @@ class PreviewApp {
         })
 
         preview.get(
-            '/posts/*',
+            '/*',
+            // '/posts/*',
             createProxyMiddleware({
-                // target: process.env.K5_PREVIEW_URL || 'http://localhost:3001',
-                target: process.env.K5_PREVIEW_URL || 'https://dev.mnews.tw',
+                target: this.proxyTarget,
                 changeOrigin: true,
                 pathRewrite: {
                     '/preview/posts': '/story',
@@ -36,9 +38,38 @@ class PreviewApp {
         return preview
     }
 
+    // nuxt page has some route also need to proxy together
+    // or preview page's source (like _nuxt folder .etc) won't have correct url path
+    handleApiAndStaticFileRouter() {
+        const nuxt = express.Router()
+
+        nuxt.get(
+            '/*',
+            createProxyMiddleware({
+                target: this.proxyTarget,
+                changeOrigin: true,
+            })
+        )
+        nuxt.post(
+            '/*',
+            createProxyMiddleware({
+                target: this.proxyTarget,
+                changeOrigin: true,
+            })
+        )
+
+        return nuxt
+    }
+
     prepareMiddleware() {
         const app = express()
         app.use(this._path, this.getPreviewRouter())
+
+        // handle nuxt website's corresponding route
+        app.use('/_nuxt', this.handleApiAndStaticFileRouter())
+        app.use('/site.webmanifest', this.handleApiAndStaticFileRouter())
+        app.use('/api', this.handleApiAndStaticFileRouter())
+
         return app
     }
 }
