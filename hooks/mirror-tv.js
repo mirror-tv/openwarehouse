f63e942157798bc0bdca5a9db0892c07cd066a7a
liyibass <liyibass@gmail.com>
2021-07-15 11:39:49 +0800

    feat: add custom ActiveFilter from app-admin-ui

--- hooks/mirror-tv.js
+++ hooks/mirror-tv.js
@@ -1,5 +1,5 @@
 // import customedItemHeaderActions from './customedItemHeaderActions';
-import React from 'react'
+import React, { Suspense } from 'react'
 import K5Logo from './components/K5Logo/K5Logo'
 import {
     ItemId,
@@ -10,8 +10,12 @@ import PreviewBtn from './components/PreviewBtn/PreviewBtn'
 import './style/hooks.style.css'
 
 import Search from './components/Search/Search'
+import ActiveFilters from './components/ActiveFilters/ActiveFilters'
 
 import { useList } from '@keystonejs/app-admin-ui/client/providers/List'
+import { useListUrlState } from '@keystonejs/app-admin-ui/client/pages/List/dataHooks'
+import { LoadingIndicator } from '@arch-ui/loading'
+import { captureSuspensePromises } from '@keystonejs/utils'
 
 export default {
     // custom landing page logo
@@ -27,9 +31,30 @@ export default {
     },
     listHeaderActions: () => {
         const { list } = useList()
+        const Render = ({ children }) => children()
+        const {
+            urlState: { currentPage, fields, pageSize, search },
+        } = useListUrlState(list)
+
         return (
             <div className="list-header-actions">
-                <Search list={list} />
+                <ActiveFilters list={list} />
+                <Suspense
+                    fallback={
+                        <LoadingIndicator css={{ height: '3em' }} size={12} />
+                    }
+                >
+                    <Render>
+                        {() => {
+                            captureSuspensePromises(
+                                fields
+                                    .filter((field) => field.path !== '_label_')
+                                    .map((field) => () => field.initCellView())
+                            )
+                            return <Search list={list} />
+                        }}
+                    </Render>
+                </Suspense>
                 <CreateItem />
             </div>
         )
