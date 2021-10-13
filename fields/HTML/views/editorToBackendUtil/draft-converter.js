39dc064dd0110a8913e621462dbc90062b0d86f4
Hsin-chan Chien <hcchien@gmail.com>
2021-10-13 23:45:37 +0800

    resolve the object of images

--- fields/HTML/views/editorToBackendUtil/draft-converter.js
+++ fields/HTML/views/editorToBackendUtil/draft-converter.js
@@ -57,11 +57,11 @@ let defaultEntityTagMap = {
         '</div>',
     ],
     [ENTITY_LIST.STOREDIMAGE.type]: [
-        '<img alt="<%= data.title %>" src="<%= data.original %>" srcset="<%= data.mobile %> 800w,  <%= data.tablet %> 1280w, <%= data.desktop %> 2400w" class="center">',
+        '<img alt="<%= data.title %>" src="<%= data.original.url %>" srcset="<%= data.mobile.url %> 800w,  <%= data.tablet.url %> 1280w, <%= data.desktop.url %> 2400w" class="center">',
         '</img>',
     ],
     [ENTITY_LIST.IMAGE.type]: [
-        '<img alt="<%= data.name %>" src="<%= data.original %>" srcset="<%= data.mobile %> 800w,  <%= data.tablet %> 1280w, <%= data.desktop %> 2400w" class="center">',
+        '<img alt="<%= data.name %>" src="<%= data.original.url %>" srcset="<%= data.mobile.url %> 800w,  <%= data.tablet.url %> 1280w, <%= data.desktop.url %> 2400w" class="center">',
         '</img>',
     ],
     /*[ENTITY_LIST.IMAGEDIFF.type]: ['<!-- imageDiff component start --> <ol class="image-diff-container"> <% _.forEach(data, function(image, index) { if (index > 1) { return; } %><li class="image-diff-item"><img src="<%- image.url %>" /></li><% }); %>', '</ol><!-- imageDiff component end-->'],
@@ -71,7 +71,7 @@ let defaultEntityTagMap = {
         '</a>',
     ],
     [ENTITY_LIST.SLIDESHOW.type]: [
-        '<!-- slideshow component start --> <ol class="slideshow-container"> <%  _.forEach(data, function(image) { %><li class="slideshow-slide"><img alt="<%- image.title %>" src="<%- image.original %>" srcset="<%= image.mobile %> 800w,  <%= image.tablet %> 1280w, <%= image.desktop %> 2400w" /></li><% }); %>',
+        '<!-- slideshow component start --> <ol class="slideshow-container"> <%  _.forEach(data, function(image) { %><li class="slideshow-slide"><img alt="<%- image.title %>" src="<%- image.original.url %>" srcset="<%= image.mobile.url %> 800w,  <%= image.tablet.url %> 1280w, <%= image.desktop.url %> 2400w" /></li><% }); %>',
         '</ol><!-- slideshow component end -->',
     ],
     [ENTITY_LIST.VIDEO.type]: [
